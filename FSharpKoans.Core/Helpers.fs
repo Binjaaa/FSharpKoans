6bdfae4ad2059332982376e7b7ea0c7881da77b3
Imre Turi <Imre.Turi@kpmg.co.uk>
2017-08-23 12:24:03 +0100

    completed tests #1

--- FSharpKoans.Core/Helpers.fs
+++ FSharpKoans.Core/Helpers.fs
@@ -4,7 +4,7 @@ module FSharpKoans.Core.Helpers
 open System
 open NUnit.Framework
 
-let inline __<'T> : 'T = failwith "Seek wisdom by filling in the __"
+let __ = "FILL ME IN"
 
 type FILL_ME_IN =
     class end
@@ -14,9 +14,9 @@ type FILL_IN_THE_EXCEPTION() =
 
 let AssertWithMessage x message = Assert.IsTrue(x, message)
 
-let AssertEquality (x:'T) (y:'T) = Assert.AreEqual(x,y)   
+let AssertEquality (x:'a) (y:'b) = Assert.AreEqual(x,y)   
 
-let AssertInequality (x:'T) (y:'T) = Assert.AreNotEqual(x,y)
+let AssertInequality (x:'a) (y:'b) = Assert.AreNotEqual(x,y)
 
 let AssertThrows<'a when 'a :> exn> action = Assert.Throws<'a>(fun () -> action())
 
